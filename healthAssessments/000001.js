d3ea34c82504a99894075d7fc31e0347ad8a1028
Andrew Osheroff <andrewosh@gmail.com>
2016-05-13 05:28:58 -0400

    Update 000001.js

--- healthAssessments/000001.js
+++ healthAssessments/000001.js
@@ -1,12 +1,14 @@
-healthAssessment: {
-  animal: 5002,
-  date: '05/12/16-19:48',
-  weightBefore: 19.3,
-  weightAfter: 21.0,
-  waterDuring: 1.7,
-  waterAfter: 0.3,
-  seeds: 1,
-  fur: 'OK'
-  posture: 'SH'
-  QHA: 0,
+{
+  healthAssessment: {
+    animal: 5002,
+    date: '05/12/16-19:48',
+    weightBefore: 19.3,
+    weightAfter: 21.0,
+    waterDuring: 1.7,
+    waterAfter: 0.3,
+    seeds: 1,
+    fur: 'OK'
+    posture: 'SH'
+    QHA: 0,
+  }
 }
